79199b7529f44e9fc8f8d2bf11352b1b658fe8d7
Noi-Git <noi.patsin@gmail.com>
2023-04-19 22:09:04 -0400

    setup server to run on a specific port

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 app.listen(PORT, console.log(`Server is runing on port ${PORT}`))
