a81246860e021f146096c85833372cf2bb10da5c
Noi-Git <noi.patsin@gmail.com>
2023-04-24 21:26:55 -0400

    add .env file and its connection to mongodb

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose')
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect('connection String', {
-      userCreateIndex: true,
-      userFindAndModify: true,
-      userUnifiedTopology: true,
-      userNewUrlParser: false,
-    })
-    console.log('If everything is good: DB connected Successful')
+    await mongoose.connect(process.env.MONGODB_URL)
+    console.log('DB connected Successful')
   } catch (error) {
     console.log(` Error ${error.message}`)
   }
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const dbConnect = require('./config/db/dbConnect')
 
 const app = express()
+
+dbConnect()
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, console.log(`Server is runing on port ${PORT}`))
