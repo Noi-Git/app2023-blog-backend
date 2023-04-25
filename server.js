71ff2a990c91cf834e2c567aec315f088888cc88
Noi-Git <noi.patsin@gmail.com>
2023-04-24 21:29:27 -0400

    see connection on the console

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const dbConnect = require('./config/db/dbConnect')
 const app = express()
 
 dbConnect()
+console.log(process.env)
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, console.log(`Server is runing on port ${PORT}`))
