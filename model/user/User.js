fecc6993dcd00e6effe16bc9d54c6f2cd57599a6
Noi-Git <noi.patsin@gmail.com>
2023-05-03 17:48:04 -0400

    use enum to assign user role

--- model/user/User.js
+++ model/user/User.js
@@ -16,4 +16,33 @@ const userSchema = new mongoose.Schema({
     default:
       'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
   },
+  email: {
+    required: [true, 'Email is required'],
+    type: Stirng,
+  },
+  bio: {
+    type: String,
+  },
+  password: {
+    required: [true, 'Password is required'],
+    type: string,
+  },
+  postCount: {
+    type: Number,
+    default: 0,
+  },
+  isBlocked: {
+    //use this in the frontend
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    //use this in the frontend
+    type: String,
+    enum: ['Admin', 'Guest', 'Blogger'], //use enum to create series of roles
+  },
 })
