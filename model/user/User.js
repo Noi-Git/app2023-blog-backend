2f1912a730bcb9bb336f81471aa3ae0b1b0960db
Noi-Git <noi.patsin@gmail.com>
2023-05-03 17:41:41 -0400

    add default avatar

--- model/user/User.js
+++ model/user/User.js
@@ -7,4 +7,13 @@ const userSchema = new mongoose.Schema({
     required: [true, 'First name is required'],
     type: String,
   },
+  lastName: {
+    required: [true, 'Last name is required'],
+    type: String,
+  },
+  profilePhoto: {
+    type: String,
+    default:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+  },
 })
