70606a110a178715c23c5cb040078242a54a1c04
Noi-Git <noi.patsin@gmail.com>
2023-05-03 21:25:10 -0400

    create post and get endpoint

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ const dbConnect = require('./config/db/dbConnect')
 const app = express()
 
 dbConnect()
-console.log(process.env)
+// console.log(process.env)
+
+//create endpoints
+//(req, res) => {} <-- this is controller where action happen
+
+//Register
+app.post('/api/users/register', (req, res) => {
+  //business logic
+  res.json({ user: 'User Register' })
+})
+
+//Login
+app.get('/api/users/login', (req, res) => {
+  //business logic
+  res.json({ user: 'User Login' })
+})
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, console.log(`Server is runing on port ${PORT}`))
