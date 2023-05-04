29dbcfa7bd5b64bcf0ea0dfe50c94af9ec22a653
Noi-Git <noi.patsin@gmail.com>
2023-05-04 19:54:30 -0400

    create notFound error handler

--- middlewares/error/errorHandler.js
+++ middlewares/error/errorHandler.js
@@ -1,3 +1,9 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${(req, originalUrl)}`) //create our own error message with new Error
+  res.status(404)
+  next(error) //send error to the next middleware - which is the errorHandler below - so the custom message we created will be apply to the error message in errHandler function
+}
+
 const errorHandler = (err, req, res, next) => {
   //needs 4 arguments
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
@@ -8,4 +14,4 @@ const errorHandler = (err, req, res, next) => {
   })
 }
 
-module.exports = { errorHandler }
+module.exports = { errorHandler, notFound }
