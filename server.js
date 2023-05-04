890bd53dd4851b5ddb3d61b90e59dccb2ff4ab98
Noi-Git <noi.patsin@gmail.com>
2023-05-04 17:09:06 -0400

    add argument to custom middleware

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ dbConnect()
 app.use(express.json())
 
 // === custom middleware ===
-const logger = () => {
+const logger = (req, res, next) => {
+  //need to call next before move on to the next middleware on the pipe line
   console.log('I am a looger')
+  next()
 }
 
 // === use custom middleware ===
