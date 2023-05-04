a11fd6a67f4752bf07972d2d1159fc4f4d21d8f3
Noi-Git <noi.patsin@gmail.com>
2023-05-03 22:02:27 -0400

    test create user logic

--- controllers/users/usersCtrl.js
+++ controllers/users/usersCtrl.js
@@ -1,5 +1,14 @@
-const userRegisterCtrl = (req, res) => {
-  //business logic
+const User = require('../../model/user/User')
+
+//Register
+const userRegisterCtrl = async (req, res) => {
+  const user = await User.create({
+    firstName: 'Emma',
+    lastName: 'Ben',
+    email: 'Emma@gmail.com',
+    password: 12345,
+  })
+
   res.json({ user: 'User Registered' })
 }
 
