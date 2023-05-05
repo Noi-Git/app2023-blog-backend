5b68734983e022c8de938d6925f176edd278ef96
Noi-Git <noi.patsin@gmail.com>
2023-05-04 20:14:10 -0400

    create route for user login

--- route/users/usersRoute.js
+++ route/users/usersRoute.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const { userRegisterCtrl } = require('../../controllers/users/usersCtrl')
+const {
+  userRegisterCtrl,
+  userLoginCtrl,
+} = require('../../controllers/users/usersCtrl')
 
 const usersRoutes = express.Router()
 
 usersRoutes.post('/register', userRegisterCtrl)
+usersRoutes.post('/login', userLoginCtrl)
 
 module.exports = usersRoutes
