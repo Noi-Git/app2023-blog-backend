d390fa83ae9d2530feebdf6429e93a1f34d4153a
Noi-Git <noi.patsin@gmail.com>
2023-05-05 00:14:29 -0400

    add jwt key

--- config/token/generateToken.js
+++ config/token/generateToken.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, 'mykeys', { expiredIn: '20d' })
+  return jwt.sign({ id }, process.env.JWT_KEY, { expiredIn: '20d' })
 }
+
+module.exports = generateToken
