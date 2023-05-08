4caed0c2165c48a1546781d6f9076721d4261126
Noi-Git <noi.patsin@gmail.com>
2023-05-08 18:50:45 -0400

    create fetch all users route

--- route/users/usersRoute.js
+++ route/users/usersRoute.js
@@ -2,11 +2,13 @@ const express = require('express')
 const {
   userRegisterCtrl,
   userLoginCtrl,
+  fetchUsersCtrl,
 } = require('../../controllers/users/usersCtrl')
 
 const usersRoutes = express.Router()
 
 usersRoutes.post('/register', userRegisterCtrl)
 usersRoutes.post('/login', userLoginCtrl)
+usersRoutes.get('/', userLoginCtrl)
 
 module.exports = usersRoutes
