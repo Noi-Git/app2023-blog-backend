fa223eb194b350d8da46c4957f9aa328b3e86b4f
Noi-Git <noi.patsin@gmail.com>
2023-05-08 21:20:04 -0400

    create fetch a single user route

--- route/users/usersRoute.js
+++ route/users/usersRoute.js
@@ -4,6 +4,7 @@ const {
   userLoginCtrl,
   fetchUsersCtrl,
   deleteUsersCtrl,
+  fetchUserDetailsCtrl,
 } = require('../../controllers/users/usersCtrl')
 
 const usersRoutes = express.Router()
@@ -11,6 +12,7 @@ const usersRoutes = express.Router()
 usersRoutes.post('/register', userRegisterCtrl)
 usersRoutes.post('/login', userLoginCtrl)
 usersRoutes.get('/', fetchUsersCtrl)
+usersRoutes.get('/:id', fetchUserDetailsCtrl)
 usersRoutes.delete('/:id', deleteUsersCtrl)
 
 module.exports = usersRoutes
