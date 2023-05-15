50c39262518428185cc6d53e7e3f9bb7c3c2b56f
Noi-Git <noi.patsin@gmail.com>
2023-05-15 19:46:47 -0400

    display error and export authMiddleware

--- middlewares/auth/authMiddleware.js
+++ middlewares/auth/authMiddleware.js
@@ -11,10 +11,16 @@ const authMiddleware = expressAsyncHandler(async (req, res, next) => {
       if (token) {
         const decoded = jwt.verify(token, process.env.JWT_KEY)
         //find user by id
-        const user = await User.findByID(decoded?.id)
+        const user = await User.findByID(decoded?.id).select('-password')
         //append user to the request object
         req.user = user
+      } else {
+        throw new Error('There is no token attatched to the header')
       }
-    } catch (error) {}
+    } catch (error) {
+      throw new Error('Not authorized, Please login again')
+    }
   }
 })
+
+module.exports = authMiddleware
