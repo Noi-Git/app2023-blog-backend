0a4e2f4ce30fa506873b4b9094febf6f465d638d
Noi-Git <noi.patsin@gmail.com>
2023-05-15 19:41:14 -0400

    find user by id using token passed to req.header

--- middlewares/auth/authMiddleware.js
+++ middlewares/auth/authMiddleware.js
@@ -6,5 +6,15 @@ const authMiddleware = expressAsyncHandler(async (req, res, next) => {
   let token
 
   if (req?.headers?.authorization?.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_KEY)
+        //find user by id
+        const user = await User.findByID(decoded?.id)
+        //append user to the request object
+        req.user = user
+      }
+    } catch (error) {}
   }
 })
