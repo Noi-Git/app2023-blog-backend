6259c60060a72b49ed8aabd2c6ccf903cd57360f
Noi-Git <noi.patsin@gmail.com>
2023-06-09 13:10:01 -0400

    create multerFilter function

--- middlewares/uploads/profilePhotoUpload.js
+++ middlewares/uploads/profilePhotoUpload.js
@@ -2,3 +2,15 @@ const multer = require('multer')
 
 //use multer storeage to temporary save our image
 const multerStorage = multer.memoryStorage()
+
+//file type checking -- cb = callback
+const multerFilter = (req, file, cb) => {
+  //check file type
+  if (file.mineType.startWith('image')) {
+    // null = successful
+    cb(null, true)
+  } else {
+    //reject file upload
+    cb({ message: 'Unsupported file format' }, false)
+  }
+}
