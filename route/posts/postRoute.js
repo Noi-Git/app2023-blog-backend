152133cc935ed6fdb276945bb16569c16d044ee5
Noi-Git <noi.patsin@gmail.com>
2023-06-09 19:57:30 -0400

    add post route

--- route/posts/postRoute.js
+++ route/posts/postRoute.js
@@ -1,5 +1,7 @@
 const express = require('express')
-
+const { createPostCtrl } = require('../../controllers/posts/postCtrl')
 const postRoute = express.Router()
+
+postRoute.post('/', createPostCtrl)
 
 module.exports = postRoute
