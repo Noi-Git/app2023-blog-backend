5d89b081e27e714a293823d5be0ba9ca8170e39b
Noi-Git <noi.patsin@gmail.com>
2023-06-09 22:24:34 -0400

    Block user

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -2,14 +2,27 @@ const expressAsyncHandler = require('express-async-handler')
 const Filter = require('bad-words')
 const Post = require('../../model/post/Post')
 const validateMongodbId = require('../../utils/validateMongodbID')
+const User = require('../../model/user/User')
 
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
+  const { _id } = req.user
   // res.json('Post controller')
   validateMongodbId(req.body.user)
   // check for bad words
   const filter = new Filter()
   const isProfane = filter.isProfane(req.body.title, req, body.description)
-  console.log(isProfane)
+  // console.log(isProfane)
+
+  //Block user
+  if (isProfane) {
+    await User.findByIdAndUpdate(_id, {
+      isBlocked: true,
+    })
+    throw new Error(
+      'Content contains profane word! Your account has been block.'
+    )
+  }
+
   try {
     const post = await Post.create(req.body)
     res.json(post)
