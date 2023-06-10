7f151b543982039348507d3b2f8d926280465d8f
Noi-Git <noi.patsin@gmail.com>
2023-06-09 23:17:08 -0400

    add postImageResize to post route

--- route/posts/postRoute.js
+++ route/posts/postRoute.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const { createPostCtrl } = require('../../controllers/posts/postCtrl')
 const authMiddleware = require('../../middlewares/auth/authMiddleware')
-const { photoUpload } = require('../../middlewares/uploads/photoUpload')
+const {
+  photoUpload,
+  postImageResize,
+} = require('../../middlewares/uploads/photoUpload')
 const postRoute = express.Router()
 
-postRoute.post('/', authMiddleware, photoUpload.single('image'), createPostCtrl)
+postRoute.post(
+  '/',
+  authMiddleware,
+  photoUpload.single('image'),
+  postImageResize,
+  createPostCtrl
+)
 
 module.exports = postRoute
