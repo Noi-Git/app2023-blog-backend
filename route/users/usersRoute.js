0941fb756c26b1148df9d396f0b3cd3f2cfe2e0e
Noi-Git <noi.patsin@gmail.com>
2023-06-09 22:37:55 -0400

    rename file to photoUpload.js

--- route/users/usersRoute.js
+++ route/users/usersRoute.js
@@ -22,7 +22,7 @@ const authMiddleware = require('../../middlewares/auth/authMiddleware')
 const {
   photoUpload,
   profilePhotoResize,
-} = require('../../middlewares/uploads/profilePhotoUpload')
+} = require('../../middlewares/uploads/photoUpload')
 const usersRoutes = express.Router()
 
 usersRoutes.post('/register', userRegisterCtrl)
