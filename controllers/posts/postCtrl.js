9f210d46574428cd0715c4e721af00cf2a35b203
Noi-Git <noi.patsin@gmail.com>
2023-06-09 20:41:18 -0400

    add validate user id and fix typo

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -1,11 +1,15 @@
 const expressAsyncHandler = require('express-async-handler')
 const Post = require('../../model/post/Post')
+const validateMongodbId = require('../../utils/validateMongodbID')
 
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
   // res.json('Post controller')
+  validateMongodbId(req.body.user)
+  console.log(req.body.user)
+
   try {
     const post = await Post.create(req.body)
-    restart.json(post)
+    res.json(post)
   } catch (error) {
     res.json(error)
   }
