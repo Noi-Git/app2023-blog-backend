1217330eb442da5c7d5d259947ba0f2e0d963362
Noi-Git <noi.patsin@gmail.com>
2023-06-09 20:15:02 -0400

    create post

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -2,7 +2,13 @@ const expressAsyncHandler = require('express-async-handler')
 const Post = require('../../model/post/Post')
 
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
-  res.json('Post controller')
+  // res.json('Post controller')
+  try {
+    const post = await Post.create(req.body)
+    restart.json(post)
+  } catch (error) {
+    res.json(error)
+  }
 })
 
 module.exports = { createPostCtrl }
