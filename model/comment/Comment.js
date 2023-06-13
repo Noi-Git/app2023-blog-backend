5b6d52584e629fc08839392ae5c399a95a6b18cf
Noi-Git <noi.patsin@gmail.com>
2023-06-13 12:10:21 -0400

    create more model properties and compile

--- model/comment/Comment.js
+++ model/comment/Comment.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose')
 
-const commentSchema = new mongoose.Schema({
-  //One to Many relationship -- one post can have manny comments
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
-    required: [true, 'Post is required'], //custom message
+const commentSchema = new mongoose.Schema(
+  {
+    //One to Many relationship -- one post can have manny comments
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: [true, 'Post is required'], //custom message
+    },
+    //One to Many relationship -- one user can create manny comments
+    user: {
+      type: Object, //user need to login in order to create post
+      required: [true, 'User is required'], //custom message
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
   },
-  //One to Many relationship -- one user can create manny comments
-  user: {
-    type: Object,
-    required: [true, 'User is required'], //custom message
-  },
-})
+  { timestamps: true } //automatically generate timestamp
+)
+
+const Comment = mongoose.model('Comment', commentSchema)
