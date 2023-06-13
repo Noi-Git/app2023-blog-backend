c9f633d4952fad907a3c1686382c55716a694874
Noi-Git <noi.patsin@gmail.com>
2023-06-13 12:52:10 -0400

    make a request to create comment

--- controllers/comments/commentCtrl.js
+++ controllers/comments/commentCtrl.js
@@ -1,4 +1,5 @@
 const expressAsyncHandler = require('express-async-handler')
+const Comment = require('../../model/comment/Comment')
 
 const createCommentCtrl = expressAsyncHandler(async (req, res) => {
   //1. get user
@@ -6,8 +7,21 @@ const createCommentCtrl = expressAsyncHandler(async (req, res) => {
   const user = req.user
 
   //2. get post id -- from request object
-  const { postId } = req.body
-  console.log(postId)
+  const { postId, description } = req.body
+  // console.log(postId)
+  console.log(description)
+
+  //make request to create comment
+  try {
+    const comment = await Comment.create({
+      post: postId,
+      user: user,
+      description: description,
+    })
+    res.json(comment)
+  } catch (error) {
+    res.json(error)
+  }
 
   res.json('Create comment')
 })
