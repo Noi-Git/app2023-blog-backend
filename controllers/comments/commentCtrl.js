fb5bd4d8cf184e5813e529fdf73fb6c88a8a0b38
Noi-Git <noi.patsin@gmail.com>
2023-06-13 12:35:06 -0400

    get user and post informantion

--- controllers/comments/commentCtrl.js
+++ controllers/comments/commentCtrl.js
@@ -1,6 +1,14 @@
 const expressAsyncHandler = require('express-async-handler')
 
 const createCommentCtrl = expressAsyncHandler(async (req, res) => {
+  //1. get user
+  // console.log(req.user) //we have access to the user because we logins
+  const user = req.user
+
+  //2. get post id -- from request object
+  const { postId } = req.body
+  console.log(postId)
+
   res.json('Create comment')
 })
 
