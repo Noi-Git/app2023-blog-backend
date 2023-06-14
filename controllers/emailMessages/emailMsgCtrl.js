b7a148a79bb1486d47f2b677853e4fa6c4fa436a
Noi-Git <noi.patsin@gmail.com>
2023-06-13 21:58:14 -0400

    add filter to prevent profane word

--- controllers/emailMessages/emailMsgCtrl.js
+++ controllers/emailMessages/emailMsgCtrl.js
@@ -1,5 +1,6 @@
 const expressAsyncHandler = require('express-async-handler')
 const sgMail = require('@sendgrid/mail')
+const Filter = require('bad-words')
 const EmailMsg = require('../../model/emailMessaging/EmailMessaging')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
@@ -7,6 +8,13 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendEmailMsgCtrl = expressAsyncHandler(async (req, res) => {
   // console.log(req.user) // see what we have under user
   const { to, subject, message } = req.body
+  const emailMessage = subject + ' ' + message
+  //prevent profanity/bad words
+  const filter = new Filter()
+
+  const isProfane = filter.isProfane(emailMessage)
+  if (isProfane) throw new Error('Email failed to send, it contains bad word')
+
   try {
     //building message
     const msg = {
@@ -20,10 +28,15 @@ const sendEmailMsgCtrl = expressAsyncHandler(async (req, res) => {
     await sgMail.send(msg)
 
     // save to database
-    res.json('Mail Sent')
+
     await EmailMsg.create({
       sentBy: req?.user?._id,
+      from: req?.user?.email,
+      to,
+      message,
+      subject,
     })
+    res.json(EmailMsg)
   } catch (error) {
     res.json(error)
   }
