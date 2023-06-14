aa92a80d4d10b539b8ea2410ea45228f43532eaf
Noi-Git <noi.patsin@gmail.com>
2023-06-13 23:00:00 -0400

    create fetch single category controller and route

--- controllers/categorys/categoryCtrl.js
+++ controllers/categorys/categoryCtrl.js
@@ -25,4 +25,20 @@ const fetchAllCateroriesCtrl = expressAsyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { createCategoryCtrl, fetchAllCateroriesCtrl }
+const fetchCategoryCtrl = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params
+  try {
+    const category = await Category.findById(id)
+      .populate('user')
+      .sort('-createdAt') //use populate to fetch actual user
+    res.json(category)
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+module.exports = {
+  createCategoryCtrl,
+  fetchAllCateroriesCtrl,
+  fetchCategoryCtrl,
+}
--- route/categorys/categoryRoute.js
+++ route/categorys/categoryRoute.js
@@ -3,11 +3,13 @@ const authMiddleware = require('../../middlewares/auth/authMiddleware')
 const {
   createCategoryCtrl,
   fetchAllCateroriesCtrl,
+  fetchCategoryCtrl,
 } = require('../../controllers/categorys/categoryCtrl')
 
 const categoryRoute = express.Router()
 
 categoryRoute.post('/', authMiddleware, createCategoryCtrl)
 categoryRoute.get('/', authMiddleware, fetchAllCateroriesCtrl)
+categoryRoute.get('/:id', authMiddleware, fetchCategoryCtrl)
 
 module.exports = categoryRoute
