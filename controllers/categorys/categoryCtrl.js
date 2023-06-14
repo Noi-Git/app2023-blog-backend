f313f4148bbd484f348fa51f902988b6f7165936
Noi-Git <noi.patsin@gmail.com>
2023-06-13 22:48:53 -0400

    use for dropdown in frontend

--- controllers/categorys/categoryCtrl.js
+++ controllers/categorys/categoryCtrl.js
@@ -3,6 +3,7 @@ const Category = require('../../model/category/Category')
 
 const createCategoryCtrl = expressAsyncHandler(async (req, res) => {
   try {
+    //create category to use for dropdown in frontend
     const category = await Category.create({
       user: req.user._id,
       title: req.body.title,
